0cb11f514a081d212d534cfaabde0533c164b2f7
Avalon111 <gc605997852@gmail.com>
2025-03-14 11:07:06 +1100

    added the new homepage
    Signed-off-by: Avalon111 <gc605997852@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+import Carousel from '../components/Carousel/Carousel'
+
+export default function HomePage() {
+  return (
+    <section>
+      <h1>Welcome to SkywardAI</h1>
+      <p>Your one-stop destination for innovative AI solutions.</p>
+      <Carousel />
+    </section>
+  )
+}
