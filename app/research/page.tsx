546e7059b4de9914d609948cab4c8a20687a5f9c
Avalon111 <gc605997852@gmail.com>
2025-03-16 21:59:17 +1100

    fixed conflict
    Signed-off-by: Avalon111 <gc605997852@gmail.com>

--- app/research/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <h1>Hello, Research and Innovation!</h1>
-  }
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 import nextra from 'nextra'
 
 const withNextra = nextra({
-  // theme: 'nextra-theme-docs',
-  // themeConfig: './theme.config.ts',
+  latex: true,
   search: {
-    codeblocks: false,
+    codeblocks: false
   },
+  contentDirBasePath: '/docs'
 })
 
 
